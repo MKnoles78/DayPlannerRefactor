8d575a52d7ce5641206049243a078be9ff8202e2
Mike Knoles <MKnoles78@gmail.com>
2021-12-14 23:06:52 -0500

    build out a table dynamically with using JavaScript

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+function myScheduleFunction() {
+
+var table = document.getElementById("mySchedule");
+var row = table.insertRow(0);
+var hour = row.insertCell(0);
+var note = row.insertCell(1);
+var saveBtn = row.insertCell(2);
+
+hour.innerHTML = "Time Goes Here";
+note.innerHTML = "Notes Go Here";
+saveBtn.innertHTML = "Button Goes Here";
+}
+
