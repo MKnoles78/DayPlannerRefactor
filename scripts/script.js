e4afde577e57a62b8089e1354fe274665aa5b02f
Mike Knoles <MKnoles78@gmail.com>
2021-12-15 00:30:33 -0500

    added id information to values

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,10 @@ var note = row.insertCell(1);
 var saveBtn = row.insertCell(2);
 
 hour.innerHTML = "Time Goes Here";
+hour.id = 'hour';
 note.innerHTML = "Notes Go Here";
+note.id = 'notes';
 saveBtn.innerHTML = "Button Goes Here";
+saveBtn.id = 'saveBtn';
 }
 
