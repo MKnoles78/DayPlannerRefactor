c3e5e896b0cfdfd8d2951c50805458571270753c
Mike Knoles <MKnoles78@gmail.com>
2021-12-16 00:27:46 -0500

    attempting to work with date and time

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,23 @@
 function myScheduleFunction() {
 
+
 var table = document.getElementById("mySchedule");
 var row = table.insertRow(0);
 var hour = row.insertCell(0);
 var note = row.insertCell(1);
 var saveBtn = row.insertCell(2);
 
+
 row.id = 'plannerRow';
-hour.innerHTML = "Time Goes Here";
 hour.id = 'hour';
-hour.className = 'col-lg-1 hour-1';
+hour.innerHTML = 'time';
+hour.className = 'col-lg-1';
 note.innerHTML = "Notes Go Here";
-note.id = 'notes';
+note.id = 'note';
+note.className = 'col-lg-10';
 saveBtn.innerHTML = "Button Goes Here";
 saveBtn.id = 'saveBtn';
+saveBtn.className = 'col-lg-1';
 }
 
+var today = Date();
