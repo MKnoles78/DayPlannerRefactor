760f46964962f75254c76dc46b8bccb9506eb887
Mike Knoles <MKnoles78@gmail.com>
2021-12-21 23:41:10 -0500

    attempted to add text area to notes

--- scripts/script.js
+++ scripts/script.js
@@ -10,13 +10,16 @@ var row = table.insertRow(0);
 var hour = row.insertCell(0);
 var note = row.insertCell(1);
 var saveBtn = row.insertCell(2);
+var noteArea = document.createElement("textarea");
 
 
 row.id = 'plannerRow';
 hour.id = 'hour';
 hour.innerHTML = 'time';
 hour.className = 'col-lg-1';
+
 note.innerHTML = "Notes Go Here";
+document.note.appendChild(noteArea);
 note.id = 'note';
 note.className = 'col-lg-10';
 saveBtn.innerHTML = "Button Goes Here";
